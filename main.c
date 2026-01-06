87897b866fac92a15083f1264be5222ba547ac4c
algasnie <gasnieralexandre@icloud.com>
2026-01-06 14:14:21 +0100

    add: function pipex_struct_init

--- main.c
+++ main.c
@@ -6,21 +6,26 @@
 /*   By: algasnie <algasnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 14:21:22 by algasnie          #+#    #+#             */
-/*   Updated: 2026/01/06 11:26:05 by algasnie         ###   ########.fr       */
+/*   Updated: 2026/01/06 14:13:38 by algasnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+static void	pipex_struct_init(t_pipex *pipex_data)
+{
+	pipex_data->fd_in = -1;
+	pipex_data->fd_out = -1;
+	pipex_data->path = NULL;
+	pipex_data->cmds = NULL;
+	pipex_data->nb_cmds = 0;
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_pipex	pipex_data;
 
-	pipex_data.fd_in = -1;
-	pipex_data.fd_out = -1;
-	pipex_data.path = NULL;
-	pipex_data.cmds = NULL;
-	pipex_data.nb_cmds = 0;
+	pipex_struct_init(&pipex_data);
 	check_args(&pipex_data, argc, envp);
 	open_fd(&pipex_data, argv);
 	get_env_path(&pipex_data, envp);
